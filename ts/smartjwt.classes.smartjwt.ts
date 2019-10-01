7900997a10ccebb3da0ad3a27c898367dbdf7461
Phil Kunz <gitlab@philkunz.com>
2019-10-01 19:58:53 +0200

    fix(core): update

--- ts/smartjwt.classes.smartjwt.ts
+++ ts/smartjwt.classes.smartjwt.ts
@@ -1,7 +1,12 @@
 import * as plugins from './smartjwt.plugins';
 
+export interface ISmartJWTJSONKeypair {
+  privatePem: string;
+  publicPem: string;
+}
+
 /**
- * 
+ * A class to create and validate JWTs and their keys
  */
 export class SmartJwt {
   public smartcryptoInstance = new plugins.smartcrypto.Smartcrypto();
@@ -40,6 +45,24 @@ export class SmartJwt {
    */
   public async setPublicKey(publicKey: plugins.smartcrypto.PublicKey) {
     this.publicKey = publicKey;
+  }
+
+  /**
+   * gets the currently set kaypair as json
+   */
+  public getKeyPairAsJson(): ISmartJWTJSONKeypair {
+    return {
+      privatePem: this.privateKey.toPemString(),
+      publicPem: this.publicKey.toPemString()
+    };
+  }
+
+  /**
+   * sets the currently set keypair as json
+   */
+  public setKeyPairAsJson(jsonKeyPair: ISmartJWTJSONKeypair) {
+    this.privateKey = plugins.smartcrypto.PrivateKey.fromPemString(jsonKeyPair.privatePem);
+    this.publicKey = plugins.smartcrypto.PublicKey.fromPemString(jsonKeyPair.publicPem);
   }
 
 
